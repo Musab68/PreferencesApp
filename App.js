7469ecf0e65d45098e1b3e2ddd7ac8a645354af8
Musab68 <musaboz109@gmail.com>
2025-12-28 13:25:37 +0300

    feat: setup stack navigation

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native'; // [cite: 73]
+import { createNativeStackNavigator } from '@react-navigation/native-stack'; // [cite: 74-75]
+import LoginScreen from './screens/LoginScreen'; // [cite: 76]
+import HomeScreen from './screens/HomeScreen'; // [cite: 77]
+import SettingsScreen from './screens/SettingsScreen'; // [cite: 78]
+
+const Stack = createNativeStackNavigator(); // [cite: 79-80]
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Settings" component={SettingsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+} // [cite: 81-90]
